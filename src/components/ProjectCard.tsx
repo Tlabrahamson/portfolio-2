eaec64a01dc4662213f703112b821914e7599afb
TimAbrahamson <tlabrahamson@yahoo.com>
2023-01-25 22:26:14 -0600

    Testing something

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ export default function ProjectCard({...props}) {
         <h4 className='small-heading'>{projectCardTitle}</h4>
         <img className='card-image' src={projectCardImage} alt={projectCardImageAlt} />
         <p>{projectCardDescription}</p>
-        <a href={projectCardLink} target='_blank' rel="noreferrer">Live Site</a>
+        {/* <a href={projectCardLink} target='_blank' rel="noreferrer">Live Site</a> */}
     </a>
   )
 }
