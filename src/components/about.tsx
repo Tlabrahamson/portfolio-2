0e3758c46deffb8f1b2d975db59355f2bfe0e495
timabrahamson <tima@ineedamaid.com>
2023-02-04 20:56:16 -0600

    Updated About content. Added temporary loader.

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default function About() {
     let currentYear = new Date().getFullYear();
     let webDevStartDate = currentYear - 2020;
-    let frameworkStartDate = currentYear - 2020;
-    let softwareStartDate = currentYear - 2021;
 
   return (
     <div className='about-container'>
         <div className='about-section'>
           <h3 className='large-heading'>About<span>:</span></h3>
           <p>
-            I'm a software developer based in Birmingham, AL. Between internships, freelance work, and full-time employment, I've been a web developer for <span>{webDevStartDate} years</span>. I've always dabbled in 
-            design, so I made it my focus to learn frontend technologies. I've been working with React and other JavaScript frameworks for <span>{frameworkStartDate} years</span> now. At my current role, I've 
-            been working with Python and Django in unison with frontend technologies for <span>{softwareStartDate} years</span>. I'm excited for the future of the web and look forward 
-            to learning more cool tech.
+            As a highly motivated software developer based in Birmingham, AL, I offer a unique blend of technical proficiency and an eye for design, 
+            having honed my skills over the course of {webDevStartDate} years in various web development capacities, including internships, freelance work, 
+            and full-time employment. My focus on frontend technologies, particularly React and JavaScript, has allowed me to bring a fresh and innovative 
+            perspective to my work. I've demonstrated my ability to adapt and manage projects outside of my comfort zone, such as maintaining a scheduling 
+            software that uses Django. Driven by a passion for the future of the web, I'm eager to take on new challenges 
+            and continue my growth as a developer by exploring cutting-edge technologies.
           </p>
         </div>
     </div>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import Spline from '@splinetool/react-spline';
 
 export default function Hero() {
+
+  const [loading, setLoading] = useState(true);
+  
 
   return (
     <div className='hero-container'>
@@ -9,7 +13,10 @@ export default function Hero() {
         </div>
         <div className='hero-spline-container'>
             <div>
-              <Spline scene="https://prod.spline.design/1DmX4NR381ndC3yM/scene.splinecode" />
+              
+                {loading && <div className='spinning-loader'></div>}
+                <Spline  onLoad={() => {setLoading(false);}}scene="https://prod.spline.design/1DmX4NR381ndC3yM/scene.splinecode" />
+              
               {/* <img src='https://res.cloudinary.com/dyhl0yyml/image/upload/q_auto/v1673495088/Portfolio/avatar_lrw0tt.webp' alt="A 3D rendering of yours truly" /> */}
             </div>
         </div>
