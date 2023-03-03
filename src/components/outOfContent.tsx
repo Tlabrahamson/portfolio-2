3f71a80a698c12ce19f3d25c6b9d75ef2da76303
timabrahamson <tima@ineedamaid.com>
2023-03-03 09:39:48 -0600

    Removed useEffect as it wasn't being used

--- src/components/outOfContent.tsx
+++ src/components/outOfContent.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function OutOfContent() {
 
