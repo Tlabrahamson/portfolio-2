f2876ab62ae6b85dca862694f6f235b741dedd21
Tim Abrahamson <tlabrahamson@yahoo.com>
2023-10-01 10:30:52 -0500

    Moved useReducer functionality into a custom hook useProjectState

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect } from 'react'
+import useProjectState from '../hooks/useProjectState';
 import axios from 'axios'
 
 type Tab = "brief" | "detailed" | "fictional";
@@ -6,26 +7,8 @@ type Tab = "brief" | "detailed" | "fictional";
 const tabs: Tab[] = ['brief', 'detailed', 'fictional'];
 
 export default function About() {
-  
-  const [state, dispatch] = useReducer((state: any, action: any) => {
-    switch(action.type) {
-      case 'SET_VISIBLE':
-        return { ...state, visible: action.visible }
-      case 'SET_LOADING':
-        return { ...state, loading: action.loading }
-      case 'SET_SELECTED':
-        return { ...state, selected: action.selected }
-      case 'SET_ERROR':
-        return { ...state, error: 'It looks like Tim should look into the paid version of OpenAI...' }
-      default: 
-        return state
-    }
-  }, {
-    visible: false,
-    error: '',
-    selected: 'detailed',
-    loading: false
-  })
+
+  const [state, dispatch] = useProjectState()
 
   const { visible, error, selected, loading} = state
 
